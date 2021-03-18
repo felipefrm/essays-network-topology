5ea925f401af0d472f33c4c4f73662551c587e0e
Felipe <narulee@hotmail.com>
2021-03-18 16:04:28 -0300

    getting weight of edge

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 import pandas as pd
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.tokenize import sent_tokenize
-from nltk.stem import RSLPStemmer
-from nltk.corpus import stopwords
-from string import punctuation
 import networkx as nx
 import matplotlib.pyplot as plt
+from PNL import *
 
 df = pd.read_csv('essays.csv')
 text = df.iloc[4]['text']
 
-words = word_tokenize(text.lower())
+words = preprocessing_text(text)
+words_link = [[words[i], words[i+1]] for i in range(len(words)-1)]
 
-punctuation_list = list(punctuation)
-punctuation_list.extend(["''", "``"])
+G = nx.DiGraph()
 
-stemmer = RSLPStemmer()
+G.add_nodes_from(words)
+G.add_edges_from(words_link)
 
-stopwords = set(stopwords.words('portuguese') + punctuation_list)
-words_filtered = [stemmer.stem(word) for word in words if word not in stopwords]
+# links_weight = Counter(words_link)  # Contains frequencies of each directed edge.
+# links_weight = [[link, words_link.count(link)] for link in words_link]
 
-edges_list = []
-for i in range(len(words_filtered)-1):
-    edges_list.append([words_filtered[i], words_filtered[i+1]])
+links_weight = dict((','.join(links), words_link.count(links)) for links in words_link) # peso das arestas
+# words_link = [list(item) for item in set(tuple(row) for row in words_link)]
 
-G = nx.Graph()
-G.add_nodes_from(words_filtered)
-G.add_edges_from(edges_list)
+# print(len(links_weight))
+# print(len(words_link))
+# print(len(list(G.edges)))
+
+for u, v, d in G.edges(data=True):
+    d['weight'] = links_weight[f'{u},{v}']
+
+print(G.out_degree('escol'))    # deveria ser 9
+print(G.in_degree('escol'))     # deveria ser 9
+
+print(G.out_degree(G.nodes()))      # tem que considerar o peso das arestas
+print(G.in_degree(G.nodes()))       # tem que considerar o peso das arestas
+print(nx.clustering(G))             # tem que considerar o peso das arestas
 
 nx.draw(G, with_labels=True, font_size='x-small')
 plt.show()
