17b29de1b16c22c8c421407b26e527a2a07cfc01
Felipe <narulee@hotmail.com>
2021-03-18 21:17:19 -0300

    weighted graph, shortest path

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 from PNL import *
 
 df = pd.read_csv('essays.csv')
 text = df.iloc[4]['text']
 
-words = preprocessing_text(text)
-words_link = [[words[i], words[i+1]] for i in range(len(words)-1)]
+nodes = preprocessing_text(text)
+edges = [[nodes[i], nodes[i+1]] for i in range(len(nodes)-1)]
 
 G = nx.DiGraph()
 
-G.add_nodes_from(words)
-G.add_edges_from(words_link)
+G.add_nodes_from(nodes)
+G.add_edges_from(edges)
 
-# links_weight = Counter(words_link)  # Contains frequencies of each directed edge.
-# links_weight = [[link, words_link.count(link)] for link in words_link]
-
-links_weight = dict((','.join(links), words_link.count(links)) for links in words_link) # peso das arestas
-# words_link = [list(item) for item in set(tuple(row) for row in words_link)]
-
-# print(len(links_weight))
-# print(len(words_link))
-# print(len(list(G.edges)))
+edges_weight = dict((','.join(edge), edges.count(edge)) for edge in edges) # peso das arestas
 
 for u, v, d in G.edges(data=True):
-    d['weight'] = links_weight[f'{u},{v}']
+    d['weight'] = edges_weight[f'{u},{v}']
 
-print(G.out_degree('escol'))    # deveria ser 9
-print(G.in_degree('escol'))     # deveria ser 9
+out_degrees = dict(G.out_degree(G.nodes(), 'weight'))
+mean_out_degrees = np.mean(list(out_degrees.values()))
+print(f'mean outdegree: {mean_out_degrees}')
 
-print(G.out_degree(G.nodes()))      # tem que considerar o peso das arestas
-print(G.in_degree(G.nodes()))       # tem que considerar o peso das arestas
-print(nx.clustering(G))             # tem que considerar o peso das arestas
+in_degrees = dict(G.in_degree(G.nodes(), 'weight'))
+mean_in_degrees = np.mean(list(in_degrees.values()))
+print(f'mean indegree: {mean_in_degrees}')
 
-nx.draw(G, with_labels=True, font_size='x-small')
-plt.show()
+clustering_coefficient = nx.clustering(G, G.nodes(), 'weight')           
+mean_clustering_coefficient = np.mean(list(clustering_coefficient.values()))
+print(f'mean clustering coefficient: {mean_clustering_coefficient}')
+
+average_shortest_path_weight = nx.average_shortest_path_length(G, 'weight')
+print(f'mean shortest path length weighted: {average_shortest_path_weight}')
+
+average_shortest_path = nx.average_shortest_path_length(G)
+print(f'mean shortest path length unweighted: {average_shortest_path}')
+
+# nx.draw(G, with_labels=True, font_size='x-small')
+# plt.show()
